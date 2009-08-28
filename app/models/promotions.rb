9629aff75050fa487cabf341d0ec581a877aa6cd
Roman Smirnov <POMAHC@gmail.com>
2009-08-28 23:58:18 +0400

    Reduce sql-queries count.

--- app/models/promotions.rb
+++ app/models/promotions.rb
@@ -5,18 +5,18 @@ class Promotions < ActiveRecord::Base
     # randomize a small list, courtesy of
     # http://redcorundum.blogspot.com/2006/12/randomizing-array-and-other-faqs.html
     promos = n <= 0 ? [] : Promotions.find(:all).sort_by {rand}[0..n-1]
-    promos.map {|p| p.product }
-  end 
+    Product.all(:conditions => ["id IN (?)", promos.map {|p| p.product_id }], :include => :images)
+  end
 
   def self.best_sellers(n, start = 1.week.ago, finish = Time.now)
     # most ordered items
     # looks for completed orders from date range (default: the past week)
     # then gets top N sellers by quantity
-    # query: need to filter out cancelled orders? 
+    # query: need to filter out cancelled orders?
     #
     # Jul09: had problem with :select vs :include in checkout_complete, hence the DIY
     best_n = Order.between(start,finish).
-                   find(:all, 
+                   find(:all,
                         :joins => "INNER JOIN checkouts ON orders.id = checkouts.order_id AND checkouts.completed_at IS NOT NULL" +
                                   " INNER JOIN line_items ON orders.id = line_items.order_id" +
                                   " INNER JOIN variants ON variant_id = variants.id",
@@ -25,6 +25,7 @@ class Promotions < ActiveRecord::Base
                         :limit => n)
 
     best_n.map {|o| [o.sum, Product.find(o.product_id)] }
-  end 
+  end
 
 end
+
